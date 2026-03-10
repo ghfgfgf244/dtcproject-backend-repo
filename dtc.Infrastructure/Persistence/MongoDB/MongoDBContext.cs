9215c81051d7d451e659834476bf277f564a3e41
HanTNNguyen2612 <trannguyenhan777@gmail.com>
2026-03-10 13:38:39 +0700

    fix XUnit test

--- dtc.Infrastructure/Persistence/MongoDB/MongoDBContext.cs
+++ dtc.Infrastructure/Persistence/MongoDB/MongoDBContext.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Driver;
+using MongoDB.Driver;
 using dtc.Domain.Entities.Blogs;
 using dtc.Domain.Entities.Exams;
 using dtc.Domain.Entities.Location;
@@ -21,20 +21,20 @@ namespace dtc.Infrastructure.Persistence.MongoDB
             _database = mongoClient.GetDatabase(mongoUrl.DatabaseName ?? "dtcproject");
         }
 
-        public IMongoCollection<Blog> Blogs => _database.GetCollection<Blog>("Blogs");
-        public IMongoCollection<Category> Categories => _database.GetCollection<Category>("Categories");
+        public virtual IMongoCollection<Blog> Blogs => _database.GetCollection<Blog>("Blogs");
+        public virtual IMongoCollection<Category> Categories => _database.GetCollection<Category>("Categories");
         
-        public IMongoCollection<SampleExam> SampleExams => _database.GetCollection<SampleExam>("SampleExams");
-        public IMongoCollection<SampleExamQuestion> SampleExamQuestions => _database.GetCollection<SampleExamQuestion>("SampleExamQuestions");
-        public IMongoCollection<Question> Questions => _database.GetCollection<Question>("Questions");
+        public virtual IMongoCollection<SampleExam> SampleExams => _database.GetCollection<SampleExam>("SampleExams");
+        public virtual IMongoCollection<SampleExamQuestion> SampleExamQuestions => _database.GetCollection<SampleExamQuestion>("SampleExamQuestions");
+        public virtual IMongoCollection<Question> Questions => _database.GetCollection<Question>("Questions");
         
-        public IMongoCollection<Address> Addresses => _database.GetCollection<Address>("Addresses");
+        public virtual IMongoCollection<Address> Addresses => _database.GetCollection<Address>("Addresses");
         
-        public IMongoCollection<Notification> Notifications => _database.GetCollection<Notification>("Notifications");
-        public IMongoCollection<NotificationRole> NotificationRoles => _database.GetCollection<NotificationRole>("NotificationRoles");
-        public IMongoCollection<UserNotification> UserNotifications => _database.GetCollection<UserNotification>("UserNotifications");
+        public virtual IMongoCollection<Notification> Notifications => _database.GetCollection<Notification>("Notifications");
+        public virtual IMongoCollection<NotificationRole> NotificationRoles => _database.GetCollection<NotificationRole>("NotificationRoles");
+        public virtual IMongoCollection<UserNotification> UserNotifications => _database.GetCollection<UserNotification>("UserNotifications");
         
-        public IMongoCollection<LearningRoadmap> LearningRoadmaps => _database.GetCollection<LearningRoadmap>("LearningRoadmaps");
-        public IMongoCollection<ResourceLearning> ResourceLearnings => _database.GetCollection<ResourceLearning>("ResourceLearnings");
+        public virtual IMongoCollection<LearningRoadmap> LearningRoadmaps => _database.GetCollection<LearningRoadmap>("LearningRoadmaps");
+        public virtual IMongoCollection<ResourceLearning> ResourceLearnings => _database.GetCollection<ResourceLearning>("ResourceLearnings");
     }
 }
